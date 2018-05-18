a796d8c3f585aa7c7e36686e24b2383a4aea52f8
Filipe Plucenio <plucenio@hotmail.com>
2018-05-17 21:27:48 -0300

    Teste de alteração de legendas Ok. Faltando refatoração de código.

--- SubtitleTimeshift/Shifter.cs
+++ SubtitleTimeshift/Shifter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -10,7 +11,7 @@ namespace SubtitleTimeshift
     {
         async static public Task Shift(Stream input, Stream output, TimeSpan timeSpan, Encoding encoding, int bufferSize = 1024, bool leaveOpen = false)
         {
-            Regex g = new Regex(@"(?<start>\S+)\s-->\s(?<end>\S+)");
+            Regex regex = new Regex(@"(?<start>\S+)\s-->\s(?<end>\S+)");
             using (StreamWriter writer = new StreamWriter(output, encoding, bufferSize, leaveOpen))
             {
                 using (StreamReader reader = new StreamReader(input, encoding, true, bufferSize, leaveOpen))
@@ -18,14 +19,14 @@ namespace SubtitleTimeshift
                     string line;
                     while ((line = reader.ReadLine()) != null)
                     {
-                        Match m = g.Match(line);
+                        Match m = regex.Match(line);
                         if (m.Success)
                         {
                             string start = m.Groups[1].Value;
                             string end = m.Groups[2].Value;
 
-                            var newStart = TimeSpan.Parse(start.Replace(",", ".")).Add(timeSpan).ToString("hh':'mm':'ss','fff");
-                            var newEnd = TimeSpan.Parse(end.Replace(",", ".")).Add(timeSpan).ToString("hh':'mm':'ss','fff");
+                            var newStart = TimeSpan.Parse(start, new CultureInfo("id-ID")).Add(timeSpan).ToString("hh':'mm':'ss'.'fff");
+                            var newEnd = TimeSpan.Parse(end, new CultureInfo("id-ID")).Add(timeSpan).ToString("hh':'mm':'ss'.'fff");
 
                             line = line.Replace(start, newStart);
                             line = line.Replace(end, newEnd);
