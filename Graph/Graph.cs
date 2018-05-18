92a7321103b1d86f9501fcd12a5450555db30bf9
Filipe Plucenio <plucenio@hotmail.com>
2018-05-18 00:28:45 -0300

    Commit de segurança.

--- Graph/Graph.cs
+++ Graph/Graph.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Graph
 {
@@ -10,14 +11,32 @@ namespace Graph
 
     public class Graph<T> : IGraph<T>
     {
+        private readonly IEnumerable<ILink<T>> _links;
+
         public Graph(IEnumerable<ILink<T>> links)
         {
-
+            _links = links;
         }
 
         public IObservable<IEnumerable<T>> RoutesBetween(T source, T target)
         {
-            throw new NotImplementedException();
+            List<T> lettersAlreadyUsed = new List<T>();
+            List<ILink<T>> NewListLinks = new List<ILink<T>>();
+            while (_links.Except(NewListLinks).Any(x => x.Source.Equals(source)))
+            {
+                var s = source;
+                foreach (var link in _links)
+                {
+                    if (link.Source.Equals(s) && !lettersAlreadyUsed.Contains(link.Target))
+                    {
+                        lettersAlreadyUsed.Add(link.Source);
+                        s = link.Target;
+                        NewListLinks.Add(link);
+                    }
+                }
+            }
+
+            return null;
         }
     }
 }
